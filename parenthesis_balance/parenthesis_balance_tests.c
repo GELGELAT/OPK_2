14bbc14f0fa612645ac3a81bee24807afb0af986
tigorsun <87004057+GELGELAT@users.noreply.github.com>
2026-09-11 10:46:29 +0300

    finished parenthesis_balance

--- parenthesis_balance/parenthesis_balance_tests.c
+++ parenthesis_balance/parenthesis_balance_tests.c
@@ -3,9 +3,10 @@
 int main(void)
 {
     char *test_strs[8] = {"", "((()))()(())", "", "()", "dasda()", "()((()))", "()()(()))", "(sad)gaf(dsad)(())"};
+    int test_value[8] = {1,1,1,1,1,1,0,1};
     for (int i = 0; i < 8; i++)
     {
         bool balanced = is_balanced(test_strs[i]);
-        printf("%d", balanced);
+        printf("test %d) current value -> %d|should be ->%d \n", i,balanced,test_value[i]);
     }
 }
