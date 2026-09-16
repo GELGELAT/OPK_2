70d0b2ba591db6339883c726c440ed1c17f56545
tigorsun <87004057+GELGELAT@users.noreply.github.com>
2026-09-16 18:54:45 +0300

    started

---
+++ selection_sort/selection_sort_scripts/selection_sort.c
@@ -0,0 +1,5 @@
+#include "selection_sort.h"
+void selection_sort(void* array,int amount,size_t size,int(*cmp(void*a,void*b,size_t size)))
+{
+
+}
---
+++ selection_sort/selection_sort_scripts/selection_sort.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stddef.h>
+
+void selection_sort(void *array, int amount, size_t size, int(*cmp(void *a, void *b, size_t size)));
