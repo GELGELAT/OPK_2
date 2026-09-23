33925c8126b65ce0f146810599a9e6c4d4f423db
tigorsun <87004057+GELGELAT@users.noreply.github.com>
2026-09-23 08:20:17 +0300

    fix

--- merge_sort/C_sort_work_lib/C_sort_work_lib_scripts/sort_work_lib.h
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_WORK_LIB_H
-#define SORT_WORK_LIB_H
-
-#include "sort_swap.h"
-
-
-#endif
--- merge_sort/merge_sort_scripts/merge_sort.h
+++ merge_sort/merge_sort_scripts/merge_sort.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stddef.h>
-#include "sort_work_lib.h"
+#include "sort_swap.h"
 
 void merge_sort(void *array, size_t amount, size_t size, int (*cmp)(const void *a, const void *b));
 
